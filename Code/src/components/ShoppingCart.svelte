d4654284e1fbe038a0d9eb7c3768eef95d9b667c
Okiki Ojo <okikioluwa.ojo@ontariotechu.net>
2022-04-05 18:30:09 +0000

    push navigation and shopping cart

--- Code/src/components/ShoppingCart.svelte
+++ Code/src/components/ShoppingCart.svelte
@@ -1,18 +1,30 @@
 <script>
-	import "fluent-svelte/theme.css";
-	import { ListItem } from "fluent-svelte";
+    import { TextBlock } from "fluent-svelte";
+	import ShoppingCartItem from "./ShoppingCartItem.svelte";
 
-    import ShoppingCartItem from "./ShoppingCartItem.svelte";
+	export let data = Array.from(Array(5), () => ({ title: "Title", cost: 5 }));
+
+	
+	/** Specifies a custom class name for the NumberBox. */
+	let className = "";
+	export { className as class };
 </script>
 
-<ul>
-    <ShoppingCartItem />
-</ul>
+<div class="shopping-cart {className ?? ''}">
+	<TextBlock variant="subtitle">Shopping Cart</TextBlock>
+	<ul class="cart-items">
+		{#each data as item}
+			<ShoppingCartItem {...item} />
+		{/each}
+	</ul>
+</div>
 
 <style>
-	:global(body) {
-		background-color: var(--fds-solid-background-base);
-		color: var(--fds-text-primary);
-        padding: 1em;
+	.shopping-cart {
+		@apply h-screen overflow-scroll;
+
+	}
+	.cart-items {
+		@apply flex flex-col gap-4 pt-2;
 	}
 </style>
--- Code/src/components/ShoppingCartItem.svelte
+++ Code/src/components/ShoppingCartItem.svelte
@@ -1,38 +1,56 @@
 <script>
 	import "fluent-svelte/theme.css";
-	import { Button, TextBox, NumberBox, TextBlock } from "fluent-svelte";
-	let value = 1;
+	import { Button, IconButton, TextBox, NumberBox, TextBlock } from "fluent-svelte";
+
+    let defaultimg = { 
+        src: "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", 
+        alt: "" 
+    };
+
+	export let amount = 1;
+    export let cost;
+    export let title; 
+
+    let image = Object.assign({}, defaultimg, $$props?.image);
+    export { image };
+
+    const { format } = new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' });
 </script>
 
 <div class="cart-item">
     <!-- https://github.com/microsoft/fluentui-system-icons -->
     <div class="image">
-        <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
+        <img {...image}>
     </div>
     <div class="content flex flex-col justify-between">
         <div class="header flex justify-between">
             <div class="flex flex-col">
-                <TextBlock variant="bodyStrong">Text</TextBlock>
-                <TextBlock class="cost" variant="body">$ 170.00</TextBlock>
+                <TextBlock variant="bodyStrong">{title}</TextBlock>
+                <TextBlock class="cost" variant="body">{format(cost)}</TextBlock>
             </div>
-            <Button class="remove-btn" aria-label="Remove Item from Shopping Cart">
+            <IconButton class="remove-btn" aria-label="Remove Item from Shopping Cart">
                 <div class="icon">
                     <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="m4.21 4.387.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12 4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" fill="currentColor"/>
                     </svg>
                 </div>
-            </Button> 
+            </IconButton> 
         </div>
 
         <div class="item-amount">
-            <TextBox type="number" bind:value />
+            <NumberBox 
+                clearButton={false} 
+                bind:value={amount} 
+            />
         </div>
     </div>
 </div>
 
 <style lang="scss">
     .cart-item {
-        @apply flex;
+        @apply flex w-full;
+        // background-color: var(--fds-subtle-fill-secondary);
+        border-radius: var(--fds-control-corner-radius);
     }
 
     .image {
@@ -45,8 +63,7 @@
     }
 
     .content {
-        @apply w-48 max-w-[theme('spacing.48')];
-        @apply p-2;
+        @apply pl-3 py-3 w-full;
 
         .cost {
             @apply tabular-nums;
